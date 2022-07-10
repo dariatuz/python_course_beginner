936c96d5c2519ed8dbc4c5ecb959d6eb4a67f35c
dariatuz <84145244+dariatuz@users.noreply.github.com>
2022-07-10 19:03:30 +0400

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,39 @@
+from typing import List
+
+a = 5 + 3
+print(a)
+
+b = 4 - 3
+print(b)
+
+c = 3 * 4
+print(c)
+
+d = 6 / 3
+print(d)
+
+e = 5 ** 2
+print(e)
+
+f = 15 % 6
+print(f)
+
+g = 3 < 4
+print(g)
+
+print(8 < 10)
+
+r = 10 // 3
+
+print(3 == 3)
+
+print(0 != 1)
+
+a -= 2
+print(a)
+
+b = 6 > 7 and 2 > -1
+print(b)
+
+cars = ['red', 'blue', 'black']
+'yellow' in cars
