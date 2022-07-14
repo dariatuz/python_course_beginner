fcfb746a47b6e3de1f709b3c1df24828fa4aeb32
dariatuz <84145244+dariatuz@users.noreply.github.com>
2022-07-14 17:44:15 +0400

    Add files via upload

---
+++ Homework3(0).py
@@ -0,0 +1,36 @@
+# задание 0: методы
+print("bye".upper())
+print("HeLP".lower())
+word = "Smart boy"
+print(word.count("m"))
+print(word.count("m",0,1))
+print(word.find("m"))
+print(word.find("l"))
+print(word.rfind("m",1))
+print(word.replace("r","l"))
+print(word.replace(" ",""))
+letters = "adfgbncktncmvk"
+print(letters.isalpha())
+numbers = "1234566"
+print(numbers.isdigit())
+print(numbers.rjust(13,"4"))
+print(numbers.ljust(13,"4"))
+split_numbers = "123,456,789"
+print(split_numbers.split(','))
+not_split = '    hello guys  \n'
+print(not_split.rstrip())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
