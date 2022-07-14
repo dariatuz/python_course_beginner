679376a15c77130dc35fa2a6954ceaae42c3a251
dariatuz <84145244+dariatuz@users.noreply.github.com>
2022-07-14 22:45:55 +0400

    Update Homework3(3).py

--- Homework3(3).py
+++ Homework3(3).py
@@ -1,16 +1,16 @@
 #task 1
-#age = 99#
-#user_name ="Ilya"
-#if  age >= 16 and age <=70:
-    #print("Welcome"+ user_name)
-    #elif age == 16:
-     #print("Dear"+ " " + user_name + "," + "you need to wait one year")
-     #elif age > 70 and age < 90:
-    #print("You are lucky"+ "," + user_name + " " +"and welcome")
-    #elif age > 90:
-    #print ("You are not real" + " " + user_name)
-    #else:
-#print("I am sorry" + " " + user_name)
+age = 99
+user_name ="Ilya"
+if  age >= 16 and age <=70:
+    print("Welcome"+ user_name)
+elif age == 16:
+     print("Dear"+ " " + user_name + "," + "you need to wait one year")
+elif age > 70 and age < 90:
+     print("You are lucky"+ "," + user_name + " " +"and welcome")
+elif age > 90:
+     print ("You are not real" + " " + user_name)
+else:
+     print("I am sorry" + " " + user_name)
 
 
 #task 2
@@ -20,9 +20,10 @@ while True:
         print("please input number!")
         continue
 if age >= int(21):
-    print("You are Alice,kiddo.")
-else:
-    print("You are not Alice,kiddo.")
+    print("You should visit Holland")
+    
+elif:
+    print("You should visit Vietnam")
 
 
 
