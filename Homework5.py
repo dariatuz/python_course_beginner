f5dfb553dbb7551e4253f263a20df307d3f794da
dariatuz <84145244+dariatuz@users.noreply.github.com>
2022-07-27 16:42:12 +0400

    Create Homework5.py

---
+++ Homework5.py
@@ -0,0 +1,26 @@
+# while 1 бесконечный цикл
+while True:
+  print("Type Ctrl-C to stop me!")
+# while 2
+x = 'polish'
+while x:
+print(x,end='')
+x = x[2:]
+# while 3
+a = 13
+b = 10
+while a < b:
+    print(a, end='')
+a += 1
+# while 4
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+    i += 1
+# while 5
+i = 5
+  while i <= 10:
+    print(i ** 2)
+    i += 1
