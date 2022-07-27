25c821f8d1f91e904a8cb7a96b9078dd8aa80fb7
dariatuz <84145244+dariatuz@users.noreply.github.com>
2022-07-27 16:45:22 +0400

    Update Homework5.py

--- Homework5.py
+++ Homework5.py
@@ -4,7 +4,7 @@ while True:
 # while 2
 x = 'polish'
 while x:
-print(x,end='')
+    print(x,end='')
 x = x[2:]
 # while 3
 a = 13
@@ -15,12 +15,12 @@ a += 1
 # while 4
 i = 1
 while i < 6:
-  print(i)
+   print(i)
   if i == 3:
     break
     i += 1
 # while 5
 i = 5
-  while i <= 10:
-    print(i ** 2)
+while i <= 10:
+   print(i ** 2)
     i += 1
